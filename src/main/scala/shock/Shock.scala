75d0341689a981a651fc4cbe6421d3e37ff0cedb
DylanGuedes <djmgguedes@gmail.com>
2018-01-12 11:16:31 -0200

    fixes websocket msg sending
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- src/main/scala/shock/Shock.scala
+++ src/main/scala/shock/Shock.scala
@@ -11,7 +11,7 @@ import org.apache.spark.sql.Dataset
 
 import org.apache.spark.sql.functions.{col}
 
-import play.api.libs.json.{JsString, Json}
+import play.api.libs.json.{JsString, Json, JsArray, JsNumber, JsObject}
 
 object Shock {
   def main(args: Array[String]) {
@@ -22,27 +22,32 @@ object Shock {
     engine.setup()
 
     val df: Dataset[Row] = engine.ingest()
-    val analytics = df.describe()
+    // val analytics = df.describe()
+    val analytics = df
     analytics.show()
-    // analytics.printSchema()
+    analytics.printSchema()
     // analytics.toJSON
     // println("analytics => ", analytics)
 
-    val conn: WS = new WS().open("ws://"+options("--ws-server")+"/socket/websocket")
+    var conn: WS = new WS().open("ws://"+options("--ws-server")+"/socket/websocket")
+    conn = conn.subscribe("analytics")
 
-    conn.subscribe("analytics")
-    analytics.columns.map({c =>
-      val values: List[Any] = analytics.select(col(c)).collect().map(r => r(0)).toList
-      val payload = Json.obj(
-        "count" -> JsString(values(0).asInstanceOf[String]),
-        "mean" -> JsString(values(1).asInstanceOf[String]),
-        "stddev" -> JsString(values(2).asInstanceOf[String]),
-        "min" -> JsString(values(3).asInstanceOf[String]),
-        "max" -> JsString(values(4).asInstanceOf[String])
-      )
-
-      conn.sendMsg("analytics:summary:"+c, payload)
+    var c: String = "temperature"
+    var values: Array[Any] = analytics.select("temperature", "created_at", "humidity").na.drop().collect().map(r => {
+      Map("temperature" -> r(0), "created_at" -> r(1), "humidity" -> r(2))
     })
+
+    var values2: Array[JsObject] = values.map((item) => {
+      val myhash: Map[String, Any] = item.asInstanceOf[Map[String, Any]]
+      Json.obj(
+        "temperature" -> JsNumber(myhash("temperature").asInstanceOf[Double]),
+        "created_at" -> JsString(myhash("created_at").toString),
+        "humidity" -> JsNumber(myhash("humidity").asInstanceOf[Double])
+      )
+    })
+    var payload = Json.obj(c -> JsArray(values2))
+    conn = conn.sendMsg("analytics", payload, "summary:"+c)
+
   }
 
   def parseOptions(args: Array[String]): Map[String, String] = {
--- src/main/scala/shock/WebSocket.scala
+++ src/main/scala/shock/WebSocket.scala
@@ -14,27 +14,17 @@ class WS() {
 
   def subscribe(topic: String): WS = {
     sendMsg(topic, Json.obj(), "phx_join")
-    this
   }
 
   def sendMsg(topic: String, payload: JsValue, event: String = "payload"): WS = {
-    var msg: JsValue = Json.obj(
+    val msg: JsValue = Json.obj(
       "topic" -> topic,
       "event" -> event,
       "payload" -> Json.stringify(payload),
       "ref" -> ""
     )
 
-    this._webSocket.send(Json.stringify(msg))
+    this._webSocket = this._webSocket.send(Json.stringify(msg))
     this
-  }
-
-  def on(event: String, callback: () => Unit) {
-    println("event happened..")
-    this._webSocket.listener(new TextListener {
-      override def onMessage(message: String) {
-        println("message => ", message)
-      }
-    })
   }
 }
