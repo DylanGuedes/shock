ca5425ec5157f69ae0e9694c47ff5d81a57070f6
DylanGuedes <djmgguedes@gmail.com>
2018-03-23 10:56:09 -0300

    add round coordinates
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- scripts/round_coordinates.py
+++ scripts/round_coordinates.py
@@ -3,9 +3,12 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 import utm
-
+import requests
+import json
 from scipy import spatial
 from xml.dom import minidom
+from pymongo import MongoClient
+import subprocess
 
 def load_nodes_from_xml(content):
     """
@@ -15,7 +18,11 @@ def load_nodes_from_xml(content):
     [[2819741.636735585, 7204646.500622427]]
     """
     nodes = content.getElementsByTagName('node')
-    reduce(lambda x, y: y.append([x.getAttribute("x"), x.getAttribute("y")]), nodes, [])
+    mylist = []
+    for u in nodes:
+        mylist.append(
+            [float(u.getAttribute("x")), float(u.getAttribute("y"))])
+    return mylist
 
 
 def coordinates_from_xml(path):
@@ -25,7 +32,8 @@ def coordinates_from_xml(path):
     >>> coordinates_from_xml("map.xml")
     [[2819741.636735585, 7204646.500622427]]
     """
-    minidom.parse(path)
+    return load_nodes_from_xml(
+            minidom.parse(path))
 
 
 def closest_point(kd_tree, point, points):
@@ -52,7 +60,7 @@ def request_buses(auth):
 
 
 def geo_to_xy(coordinate):
-    x, y, _, _ = utm.from_latlon(coordinate["px"], coordinate["py"])
+    x, y, _, _ = utm.from_latlon(coordinate[0], coordinate[1])
     return [x,y]
 
 
@@ -60,12 +68,37 @@ def bus_data_from_geo_to_xy(data):
     return map(geo_to_xy, data)
 
 
+mytoken = subprocess\
+            .Popen(["cat", "/tmp/.olho_vivo_api"], stdout=subprocess.PIPE)\
+            .stdout\
+            .read()\
+            .strip()
+
 auth = olho_vivo_get_auth(mytoken)
 buses_data = request_buses(auth)
-
-transformed_buses = bus_data_from_geo_to_xy(buses_data['l'])
-
-
 pts = coordinates_from_xml("map.xml")
 tree = mount_kd_tree(pts)
-closest_point(tree, mypoint, pts)
+client = MongoClient()
+db = client['sp']
+collection = db['olho_vivo']
+
+
+for bus_line in buses_data['l']:
+    for vehicle in bus_line['vs']:
+        rounded_point = closest_point(
+            tree, geo_to_xy([vehicle['px'], vehicle['py']]), pts)
+        doc = {
+            'prefix': vehicle['p'],
+            'lat': vehicle['py'],
+            'lon': vehicle['px'],
+            'timestamp': vehicle['ta'],
+            'display': bus_line['c'],
+            'line_identifier': bus_line['cl'],
+            'direction': bus_line['sl'],
+            'display_origin': bus_line['lt0'],
+            'display_destination': bus_line['lt1'],
+            'vehicles_count': bus_line['qv'],
+            'rounded_x': rounded_point[0],
+            'rounded_y': rounded_point[1]
+        }
+        collection.insert_one(doc)
